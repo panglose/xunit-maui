a9eda2058d5426693a03f5e9bf82e874c6007cba
Anben PANGLOSE <anben.panglose@infoscape.onmicrosoft.com>
2024-05-24 10:53:42 +0200

    passage sous net 9

--- xunit.runners.maui/AssertionExtensions.cs
+++ xunit.runners.maui/AssertionExtensions.cs
@@ -2,29 +2,29 @@ using Xunit.Sdk;
 
 namespace Xunit.Runners.Maui
 {
-	public static partial class AssertionExtensions
-	{
-		static readonly Random rnd = new Random();
+    public static partial class AssertionExtensions
+    {
+        static readonly Random rnd = new Random();
 
-		public static async Task<bool> Wait(Func<bool> exitCondition, int timeout = 1000)
-		{
-			while ((timeout -= 100) > 0)
-			{
-				if (!exitCondition.Invoke())
-					await Task.Delay(rnd.Next(100, 200));
-				else
-					break;
-			}
+        public static async Task<bool> Wait(Func<bool> exitCondition, int timeout = 1000)
+        {
+            while ((timeout -= 100) > 0)
+            {
+                if (!exitCondition.Invoke())
+                    await Task.Delay(rnd.Next(100, 200));
+                else
+                    break;
+            }
 
-			return exitCondition.Invoke();
-		}
+            return exitCondition.Invoke();
+        }
 
-		public static void AssertHasFlag(this Enum self, Enum flag)
-		{
-			var hasFlag = self.HasFlag(flag);
+        public static void AssertHasFlag(this Enum self, Enum flag)
+        {
+            var hasFlag = self.HasFlag(flag);
 
-			if (!hasFlag)
-				throw new ContainsException(flag, self);
-		}
-	}
+            if (!hasFlag)
+                throw ContainsException.ForCollectionItemNotFound($"flag {flag}", $"Enum {self.GetType().Name}");
+        }
+    }
 }
